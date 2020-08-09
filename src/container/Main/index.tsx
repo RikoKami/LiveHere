6ecd3219333d38fcfcd7ffe31a89265937f7adba
Larissa <orglary@gmail.com>
2020-08-08 21:05:02 -0300

    Feat: configuring the inicial structure the project

---
+++ src/container/Main/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <p>
+        Aqui vai vim as rotas em algum momento{" "}
+        <span role="img" aria-label="happy">
+          😀
+        </span>
+      </p>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./container/Main";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
